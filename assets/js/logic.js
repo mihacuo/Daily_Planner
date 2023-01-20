00effeb2251d09616a082c130dc3dd3fd127b7e4
mihacuo <mihhail.odnolko@gmail.com>
2023-01-20 22:38:34 +0000

    future present past colours work

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,8 @@
 console.log("test");
 
 // display current date & time
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+var timeStamp = moment().format("dddd, MMMM Do YYYY, h:mm a")
+$("#currentDay").text(timeStamp);
 
 //gets current hour
 var currentHR = moment().format("ha");
@@ -25,27 +26,40 @@ var data = [
 // current time instead of morning
 // temporary variable
 var data = [
-  { hr: "4pm" },
-  { hr: "5pm" },
-  { hr: "6pm" },
-  { hr: "7pm" },
+
   { hr: "8pm" },
   { hr: "9pm" },
   { hr: "10pm" },
+   {hr: "11pm"}
 ];
 
 function renewData() {
   $(".container").children().remove();
   id = 0;
-  currentClass = "past";
+
+  //let's get current time in unix format
+  var nowHR = moment().format("ha");
+  var nowUnix = parseInt(moment(nowHR, "ha").format("X"));
+  //console.log(typeof(nowUnix))
+  console.log('noWunix' , nowUnix);
   data.forEach(function (element) {
-    element["id"] = id;
-    element["classAssign"] = currentClass;
-    if (currentHR == element["hr"]) {
-      element["classAssign"] = "present";
-      currentClass = "future";
+    //let's get unix time of element's hour
+    var thisElementHR = moment(element.hr, "ha");
+    var thisElementHRUnix = parseInt(thisElementHR.format("X"));
+    console.log(thisElementHRUnix)
+    requiredClass = "present"
+    if (nowUnix > thisElementHRUnix) {
+      requiredClass = "past"
+    } else if (nowUnix < thisElementHRUnix) {
+      requiredClass = "future"
     }
+    //add class name to properties
+    element["classAssign"] = requiredClass;
+    //not sure what for, but will add id's in case
+    element["id"] = "textarea-" + id;
     id++;
+
+    //let's generate html of the time blocks.
     var wholeRow = $('<div>');
     wholeRow.addClass("whole-row")
     var timeBlock = $('<div>');
@@ -53,18 +67,13 @@ function renewData() {
     var hrSection= $("<span>");
     hrSection.addClass("hour")
     hrSection.text(element["hr"]);
-    var area = $("<span>");
-    area.text= "dummy text";
     var btn = $("<button>");
-    btn.text = "save!";
     btn.addClass("saveBtn");
     var saveBTN = $("<button>");
     saveBTN.addClass("SaveBtn");
     var img = $("<img id='save' height='35px' width='35px' src='./assets/img/floppy.jpg'/>")
     saveBTN.append(img);
-
     timeBlock.append(hrSection);
-
     timeBlock.addClass(element["classAssign"])
     wholeRow.append(hrSection, timeBlock, saveBTN)
     $(".container").append(wholeRow);
